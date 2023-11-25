80336849f669c346e4a2b5c38a7b360c9f594a8e
eoblue <eoblue22@naver.com>
2023-11-25 21:20:58 +0900

    knu_counter_status db 내용 받아오기

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
+import pymysql # mysql 모듈
+
+# mysql db 연결
+db = pymysql.connect(host='180.66.240.165', port=53306, user='root', password='U6ycE],+', db='xedb', charset='utf8')
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-
 @bp.route('/')
 def index():
-    return render_template('home.html')
+    cursor = db.cursor()  # db 데이터 가르킬 객체 생성
+    sql = "SELECT * from knu.knu_counter_status"
+    cursor.execute(sql)
+    data_list = cursor.fetchall()
+    return render_template('home.html', data_list=data_list)
