e18d51025f4f97c6cabe7b9ec545b3db1ba09eb2
anheejeong <eyrt6973@naver.com>
2023-12-06 18:35:03 +0900

    add wordcloud chart in NLP page

--- src/pages/notifications/Notifications.js
+++ src/pages/notifications/Notifications.js
@@ -18,6 +18,8 @@ import echarts from "echarts/lib/echarts";
 
 import $ from "jquery";
 
+import ReactWordcloud from 'react-wordcloud';
+
 exporting(Highcharts);
 exportData(Highcharts);
 
@@ -84,6 +86,36 @@ class Notifications extends React.Component {
 
     console.log(this.state.cd.echarts.nodes)
     console.log(this.state.cd.echarts.categories)
+
+    const words = [
+      {
+        text: 'told',
+        value: 120,
+      },
+      {
+        text: 'mistake',
+        value: 110,
+      },
+      {
+        text: 'thought',
+        value: 160,
+      },
+      {
+        text: 'bad',
+        value: 170,
+      },
+    ]
+    const callbacks = {
+      getWordColor: word => word.value > 50 ? "white" : "red",
+      onWordClick: console.log,
+      onWordMouseOver: console.log,
+      getWordTooltip: word => `${word.text} (${word.value}) [${word.value > 50 ? "good" : "bad"}]`,
+    }
+    const options = {
+      rotations: 2,
+      rotationAngles: [-90, 0],
+    };
+    // const size = [600, 400];
 
     const clustering = {
       tooltip: {},
@@ -157,6 +189,40 @@ class Notifications extends React.Component {
                 option={clustering}
                 opts={initEchartsOptions}
                 style={{ height: 700 }}
+              />
+            </Widget>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={6} xs={12}>
+            <Widget
+              title={
+                <h5>정회원 WordCloud</h5>
+              }
+              close
+              collapse
+            >
+              <ReactWordcloud
+                callbacks={callbacks}
+                options={options}
+                // size={size}
+                words={words}
+              />
+            </Widget>
+          </Col>
+          <Col lg={6} xs={12}>
+            <Widget
+              title={
+                <h5>비회원 WordCloud</h5>
+              }
+              close
+              collapse
+            >
+              <ReactWordcloud
+                callbacks={callbacks}
+                options={options}
+                // size={size}
+                words={words}
               />
             </Widget>
           </Col>
